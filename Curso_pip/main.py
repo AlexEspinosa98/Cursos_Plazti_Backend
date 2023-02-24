7f8d1493c29e6b5a57355616c520c3ae65f6290c
Alex_Espinosa <espinosaalexander09@gmail.com>
2023-02-24 12:17:35 -0500

    :rotating_light: creando mi primera petición con request

---
+++ Curso_pip/main.py
@@ -0,0 +1,8 @@
+import web_server
+
+
+def run():
+    web_server.get_categories()
+
+if __name__ == '__main__':
+    run()
---
+++ Curso_pip/web_server.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def get_categories():
+    r=requests.get("https://api.escuelajs.co/api/v1/categories")
+    #print(r.statatus_code)
+
+    print(f"status =  {r.status_code}")
+    print(f"Texto = {r.text}")
+    print(f" tipo = {type(r.text)}")
+    categories= r.json()
+    print("*-"*15)
+    for cate in categories:
+        print(cate["name"])
